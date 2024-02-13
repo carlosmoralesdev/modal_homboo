eaac8ca1fe1260460a1bb51727228951ab2668b6
Morales <Carlos.Morales@seguroscaracas.com>
2024-02-13 00:05:54 -0400

    cambio resize

--- modalResize.js
+++ modalResize.js
@@ -4,7 +4,8 @@ function changeHeight() {
     array = document.getElementsByClassName('modal-body')
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
-        elemento.style.maxHeight = "calc(" + valorHeight + "px - 180px) !important"
+        elemento.style.height = (valorHeight - 180) + "px"
+        console.log(elemento)
     }
     /* elementos.forEach(elemento => {
         elemento.style.maxHeight = "calc(" + valorHeight + " - 180px) !important"
