56c300e4cb3e7d78ffe59c8fbeda598b8df24081
Morales <Carlos.Morales@seguroscaracas.com>
2024-02-13 00:16:00 -0400

    homboo ajuste resize

--- modalResize.js
+++ modalResize.js
@@ -4,8 +4,8 @@ function changeHeight() {
     array = document.getElementsByClassName('modal-body')
     for (let index = 0; index < array.length; index++) {
         let elemento = array[index]
-        elemento.style.height = (valorHeight - 180) + "px"
-        console.log(elemento)
+        elemento.style.height = (valorHeight - 200) + "px"
+        //console.log(elemento)
     }
     /* elementos.forEach(elemento => {
         elemento.style.maxHeight = "calc(" + valorHeight + " - 180px) !important"
