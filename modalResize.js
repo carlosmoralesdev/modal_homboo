2e5289432d1265cd4cccf826ec55605f7d5c8c7c
Morales <Carlos.Morales@seguroscaracas.com>
2024-02-12 23:28:55 -0400

    first commit

---
+++ modalResize.js
@@ -0,0 +1,15 @@
+function changeHeight() {
+    let valorHeight = window.innerHeight
+    let array = []
+    array = document.getElementsByClassName('modal-body')
+    for (let index = 0; index < array.length; index++) {
+        let elemento = array[index]
+        elemento.style.maxHeight = "calc(" + valorHeight + " - 180px) !important"
+    }
+    /* elementos.forEach(elemento => {
+        elemento.style.maxHeight = "calc(" + valorHeight + " - 180px) !important"
+    }) */
+}
+
+window.addEventListener("resize", changeHeight);
+changeHeight()
